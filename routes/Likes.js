795bcb3b568818a5806ef347e9c3fad7ecf7a76a
Siddhant Raj <siddhantraj04@gmail.com>
2023-06-11 22:49:13 +0530

    deploy database

--- routes/Likes.js
+++ routes/Likes.js
@@ -8,7 +8,7 @@ dotenv.config();
 const db=mysql.createPool({
     host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD === 'none' ? '' : process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD ,
   database: process.env.DB_DATABASE
 })
 
