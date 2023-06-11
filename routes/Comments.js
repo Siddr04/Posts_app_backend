aa4d3bb0aa3abe5d3270ff15752fbc3c6552e3f9
Siddhant Raj <siddhantraj04@gmail.com>
2023-06-11 22:08:03 +0530

    commit

--- routes/Comments.js
+++ routes/Comments.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const dotenv=require('dotenv');
 dotenv.config();
 const db=mysql.createPool({
-    host: process.env.DB_HOST,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD === 'none' ? '' : process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE
